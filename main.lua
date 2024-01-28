07590888433ef8147d886c71a310ed3f924d9224
deawenism <46120875+deawenism@users.noreply.github.com>
2024-01-28 14:28:53 +1100

    copied from mods folder

--- main.lua
+++ main.lua
@@ -2,11 +2,11 @@ local PencilCounter = RegisterMod("PencilCounter",1)
 local counter = 0
 local hasShot = false
 PencilCounter.COLLECTIBLE_LEAD_PENCIL = Isaac.GetItemIdByName("Lead Pencil")
-
+local myEntity = nil
 -- initialises variables on start   
 function PencilCounter:onInit()
     counter = 0
-    hasShot = false
+    hasShot = false    
 end
 -- counts shots
 function PencilCounter:onCount()  
@@ -20,11 +20,25 @@ function PencilCounter:onCount()
 end
 function PencilCounter:OnRender()
     if player:HasCollectible(PencilCounter.COLLECTIBLE_LEAD_PENCIL) then 
-        Isaac.RenderText(counter, 150, 20, 1, 1, 1,1)
-    end   
+        if myEntity == nil then
+            myEntity = Isaac.Spawn(65592309, 0,0, player.Position,  Vector(0,0), player)
+            myEntity:AddEntityFlags(EntityFlag.FLAG_FRIENDLY|EntityFlag.FLAG_PERSISTENT)
+            myEntity.PositionOffset = Vector(0,-50)
+            print(myEntity)
+        end
+        if myEntity ~= nil then
+            myEntity.Position = player.Position
+            myEntity:GetSprite():SetFrame(counter)
+        end
+       
+    else
+        myEntity = nil
+    end
     hasShot = false
 end
 
 PencilCounter:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, PencilCounter.onCount)
 PencilCounter:AddCallback(ModCallbacks.MC_POST_RENDER, PencilCounter.OnRender)
 PencilCounter:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, PencilCounter.onInit)
+
+
